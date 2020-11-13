3d9976906b3ab09334434573eb17704bc52e6c0c
cit-arceo <citlalliarceo@gmail.com>
2020-11-13 17:12:18 -0600

    add ui - red #23

---
+++ src/app/ui/ui.component.spec.ts
@@ -0,0 +1,200 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UiComponent } from './ui.component';
+
+describe('UiComponent', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('Ui Addition - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call addition method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render sum in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Substraction - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call substraction method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render subs in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Multiplication - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call multiplication method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render mul in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Division - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call division method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render div in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Exp - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call exp method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render exp in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Sqr - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call sqr method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render sqr in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
+
+describe('Ui Sqrt - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call sqrt method', () => {
+    expect(component).toBeFalsy();
+  });
+  it('Should render sqrt in TxtResut', () => {
+    expect(component).toBeFalsy();
+  });
+});
